2478e5b299fac7b23cb9ee179f17fcefe8b017cd
Krystian Szmida <krystianszm123@gmail.com>
2021-11-18 21:17:25 +0100

    Add Init_project.py

---
+++ Zadanie.py
@@ -0,0 +1,7 @@
+print("Lista zakupów")
+
+shopping = {"piekarnia": "chleb, bułki, pączek", "warzywniak": "marchew, seler, rukola"}
+shopping_dict = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+    }
